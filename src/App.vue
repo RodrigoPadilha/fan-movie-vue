ca1ff9b7439533d86eb3f7478517d10c948871a9
Rodrigo Padilha <rodrigo.padilha@marcopolo.com.br>
2020-08-23 15:37:33 -0300

    Implementado componente Card par aexibiçao de posters de filmes

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,100 @@
 <template>
-  <div>    
-    <ul>
-        <li v-for="item of lista" :key="item.id">
-          <div>
-              <h2>{{item.original_title}}</h2>
-              <img :src="'http://image.tmdb.org/t/p/w45/' + item.poster_path" :alt="item.title">
-              <img :src="'http://image.tmdb.org/t/p/w92/' + item.poster_path" :alt="item.title">
-              <img :src="'http://image.tmdb.org/t/p/w154/' + item.poster_path" :alt="item.title">
-              <img :src="'http://image.tmdb.org/t/p/w185/' + item.poster_path" :alt="item.title">
-              <img :src="'http://image.tmdb.org/t/p/w300/' + item.poster_path" :alt="item.title">              
-              <img :src="'http://image.tmdb.org/t/p/w500/' + item.poster_path" :alt="item.title">              
-          </div>
-        </li>
 
+  <div>    
+    
+    <ul class="lista-filmes">
+        <li class="lista-filmes__item" v-for="item of lista" :key="item.id">        
+
+            <card-filme :titulo="item.original_title">
+                <img class="card__poster" :src="'http://image.tmdb.org/t/p/w185/' + item.poster_path" :alt="item.title"> <!-- w45, w92, w154, w300, w500-->                              
+            </card-filme>
+
+        </li>
     </ul>
+
   </div>
 
 
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data () {
-      return {
-        lista: []   
-      }    
-    },
+    import Card from './components/card/Card.vue';    
 
-    created() {
-      const axios = require('axios');
-      /*
-      const instance = axios.create({
-        baseUrl: 'https://api.themoviedb.org/3/',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDFlYWVjODJmMzE1ZjI0ZDg5ZjVjZWExMGY5YzQ2ZSIsInN1YiI6IjViNWE1ZjlhOTI1MTQxMmE5ZjAwMTA5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XG68mjoy-VhwCmHTJ4xFw7x_VdjmL4M5STBLuYOTV24'
+    export default {
+
+        components: {
+            'card-filme' : Card,    //usar aspas simples pois o nome "card-filme" possui ifem
+            //card: Card
+        },
+
+        name: 'app',
+        data () {
+            return {
+            lista: []   
+            }    
+        },
+
+        created() {
+            const axios = require('axios');
+            /*
+            const instance = axios.create({
+                baseUrl: 'https://api.themoviedb.org/3/',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDFlYWVjODJmMzE1ZjI0ZDg5ZjVjZWExMGY5YzQ2ZSIsInN1YiI6IjViNWE1ZjlhOTI1MTQxMmE5ZjAwMTA5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XG68mjoy-VhwCmHTJ4xFw7x_VdjmL4M5STBLuYOTV24'
+                }
+            })
+            */
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDFlYWVjODJmMzE1ZjI0ZDg5ZjVjZWExMGY5YzQ2ZSIsInN1YiI6IjViNWE1ZjlhOTI1MTQxMmE5ZjAwMTA5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XG68mjoy-VhwCmHTJ4xFw7x_VdjmL4M5STBLuYOTV24'
+            }
+            axios.get('https://api.themoviedb.org/3/movie/upcoming', { headers })
+                .then(response => response.data.results )
+                .then(filmes => {
+                    console.log(filmes);
+                    this.lista = filmes;
+                })
+                .catch(error => {          
+                    console.error(error);          
+                })
         }
-      })
-      */
-      const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDFlYWVjODJmMzE1ZjI0ZDg5ZjVjZWExMGY5YzQ2ZSIsInN1YiI6IjViNWE1ZjlhOTI1MTQxMmE5ZjAwMTA5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XG68mjoy-VhwCmHTJ4xFw7x_VdjmL4M5STBLuYOTV24'
-      }
-      axios.get('https://api.themoviedb.org/3/movie/upcoming', { headers })
-        .then(response => response.data.results )
-        .then(filmes => {
-          console.log(filmes);
-          this.lista = filmes;
-        })
-        .catch(error => {          
-          console.error(error);          
-        })
     }
-  }
 
 </script>
 
 <style>
+
+    .lista-filmes{
+        list-style: none;
+    }
+
+    .lista-filmes__item{
+        display: inline-block;
+    }
+
+    .card {    
+        vertical-align: top;
+        text-align: center;
+
+        padding: 0 auto;
+        margin: .8em;
+        /*border: solid 2px grey;*/
+        border-radius: .5em .5em;
+
+        box-shadow: .5em .5em 1em grey;
+        display: inline-block;    
+    }
+
+    .card__poster{
+        width: 100%;
+        border-radius: .5em .5em;
+    }
+    
+    .card__titulo {
+        text-align: center;                
+        margin: 0;
+        padding: 0 .5em;                   
+    }
 
 </style>
---
+++ src/components/card/Card.vue
@@ -0,0 +1,54 @@
+<template>
+    
+    <div class="card">                
+        <div class="card__conteudo">
+            <!--<img class="card__poster" :src="'http://image.tmdb.org/t/p/w185/' + item.poster_path" :alt="item.title">--> <!-- w45, w92, w154, w300, w500-->
+            <div class="card__info">                
+                <h2 class="card__titulo">{{ titulo }}</h2>              
+                <!--
+                <p>Data:01/01/2021</p>
+                <p>Gênero: abc</p>
+                -->
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+  
+    export default {
+
+        props:['titulo']
+
+    }
+
+</script>
+
+<style>
+
+    .card {    
+        vertical-align: top;
+        text-align: center;
+
+        padding: 0 auto;
+        margin: .8em;
+        border-radius: .5em .5em;
+
+        box-shadow: .5em .5em 1em grey;
+        display: inline-block;    
+    }
+
+    .card__poster{
+        width: 100%;
+        border-radius: .5em .5em;
+    }
+    
+    .card__titulo {
+        text-align: center;                
+        margin: 0;
+        padding: 0 .5em;                   
+    }
+
+</style>
